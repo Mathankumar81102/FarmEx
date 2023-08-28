5ccaa3d07126001f25377969a8418571eecd9bab
Lokesh <lokeshtrt0108@gmail.com>
2023-08-28 17:46:36 +0530

    Model predicted the disease

--- Backend/Backend-Model/app.py
+++ Backend/Backend-Model/app.py
@@ -4,6 +4,11 @@ import os
 import glob
 import re
 import numpy as np
+import keras
+import tensorflow as tf
+
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications import imagenet_utils
 
 from keras.applications.imagenet_utils import preprocess_input,decode_predictions
 from keras.models import load_model
@@ -15,39 +20,83 @@ from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
-#MODEL_PATH = 'models/model_resnet.h5'
-#model = load_model(MODEL_PATH)
-#mode._make_predict_function()
+vclass_names = ['Apple___Apple_scab',
+     'Apple___Black_rot',
+     'Apple___Cedar_apple_rust',
+     'Apple___healthy',
+     'Blueberry___healthy',
+     'Cherry_(including_sour)___Powdery_mildew',
+     'Cherry_(including_sour)___healthy',
+     'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+     'Corn_(maize)__Common_rust',
+     'Corn_(maize)___Northern_Leaf_Blight',
+     'Corn_(maize)___healthy',
+     'Grape___Black_rot',
+     'Grape__Esca(Black_Measles)',
+     'Grape__Leaf_blight(Isariopsis_Leaf_Spot)',
+     'Grape___healthy',
+     'Orange__Haunglongbing(Citrus_greening)',
+     'Peach___Bacterial_spot',
+     'Peach___healthy',
+     'Pepper,bell__Bacterial_spot',
+     'Pepper,bell__healthy',
+     'Potato___Early_blight',
+     'Potato___Late_blight',
+     'Potato___healthy',
+     'Raspberry___healthy',
+     'Soybean___healthy',
+     'Squash___Powdery_mildew',
+     'Strawberry___Leaf_scorch',
+     'Strawberry___healthy',
+     'Tomato___Bacterial_spot',
+     'Tomato___Early_blight',
+     'Tomato___Late_blight',
+     'Tomato___Leaf_Mold',
+     'Tomato___Septoria_leaf_spot',
+     'Tomato___Spider_mites Two-spotted_spider_mite',
+     'Tomato___Target_Spot',
+     'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+     'Tomato___Tomato_mosaic_virus',
+     'Tomato___healthy']
 
-# def model_predict(img_path,model):
-#     img = image.load_img(img_path,target_size = (224,224))
-#     x = image.img_to_array(img)
-#     x = np.expand_dims(x,axis=0)
-#     x = preprocess_input(x,mode='caffe')
-#     preds = model.predict(x)
-#     return preds
+MODEL_PATH = 'Models\plant_disease_detector_mobilenet.h5'
+model = load_model(MODEL_PATH)
 
+model.make_predict_function()
+def model_predict(img_path,model):
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
+    # print(img_array_expanded_dims.shape)
+    x = tf.keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
+    preds = tf.nn.softmax(model.predict(x))
+    pred_class = vclass_names[np.argmax(preds)]
+    print(type(pred_class))
+    return pred_class
 
-# @app.route('/predict',method = ['GET','POST'])
-# def upload():
-#     if request.method == 'POST':
-#         f = request.files['file']
+@app.route('/',methods = ['GET','POST'])
+def slash():
+    return "Hi"
 
-#         basepath = os.path.dirname(__file__)
-#         file_path = os.path.join(
-#             basepath, 'uploadImages', secure_filename(f.filename)
-#         )
-#         f.save(file_path)
-#         preds = model_predict(file_path,model)
+@app.route('/predict',methods = ['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        f = request.files['file']
 
-#         pred_class = decode_predictions(preds,top=1)
-#         result = str(pred_class[0][0][1])
-#         return result
-#     return None
-
-# @app.route("/members")
-# def members():
-#     return {"members" : ["m1","m2","m3"]}
+        basepath = os.path.dirname(__file__)
+        
+        # Check the directory structure and paths
+        upload_dir = os.path.join(basepath, 'uploadImages')
+        if not os.path.exists(upload_dir):
+            os.makedirs(upload_dir)
+        
+        # Construct the full file path
+        file_path = os.path.join(upload_dir, secure_filename(f.filename))
+        f.save(file_path)
+        
+        preds = model_predict(file_path, model)
+        return preds
+    return "Prediction endpoint. Send a POST request with an image file."
 
 if __name__ == "__main__":
     app.run(debug=True)
